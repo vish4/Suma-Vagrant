3718d630d038aac42d9b7f52a434a411394e0451
vish4 <vish.n412@gmail.com>
2016-04-21 15:12:25 -0400

    Update DockerStart.sh

--- DockerStart.sh
+++ DockerStart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ ! -d "suma" ]; then
-  # Control will enter here if $DIRECTORY exists.
+  # Control will enter here if $DIRECTORY does not exists.
  sudo git clone https://github.com/cazzerson/Suma.git suma
 fi
 sudo docker-compose up -d
