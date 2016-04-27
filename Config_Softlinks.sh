0be1e089f18b77aaf05faf6045f5786356c21218
vsnarvek <vsnarvek@ncsu.edu>
2016-04-27 12:08:33 -0400

    Fix for MAC

--- Config_Softlinks.sh
+++ Config_Softlinks.sh
@@ -6,9 +6,6 @@ ln -s /vagrant/suma/service/web/ /var/www/html/sumaserver
 mkdir -p /var/www/html/suma/
 ln -s /vagrant/suma/web/ /var/www/html/suma/client
 ln -s /vagrant/suma/analysis/ /var/www/html/suma/analysis
-mkdir /vagrant/suma/sumalogs/
-touch /vagrant/suma/sumalogs/sumaserver.log
-chmod 666 /vagrant/suma/sumalogs/sumaserver.log
 
 cp /vagrant/configs/index.html /var/www/html
 
@@ -25,9 +22,3 @@ a2dismod mpm_event
 a2enmod mpm_prefork
 a2enmod rewrite
 /etc/init.d/apache2 restart
-
-ln -sf /vagrant/configs/service/config/config.yaml /vagrant/suma/service/config/config.yaml
-ln -sf /vagrant/configs/service/web/config/config.yaml /vagrant/suma/service/web/config/config.yaml
-ln -sf /vagrant/configs/analysis/config/config.yaml /vagrant/suma/analysis/config/config.yaml
-ln -sf /vagrant/configs/web/config/spaceassessConfig.js /vagrant/suma/web/config/spaceassessConfig.js
-
--- DockerStart.sh
+++ DockerStart.sh
@@ -1,6 +1,36 @@
 #!/usr/bin/env bash
+
+#cd /tmp
+#echo "Override docker upstart init config"
+#echo "start on (vagrant-mounted and filesystem and net-device-up IFACE!=lo)" > /etc/init/docker.override
+
+#echo "Installing docker-compose..."
+#wget https://bootstrap.pypa.io/get-pip.py
+#python get-pip.py
+
+#/usr/local/bin/pip install -U docker-compose twarc
+
+#cd /vagrant
+
 if [ ! -d "suma" ]; then
-  # Control will enter here if $DIRECTORY does not exists.
- sudo git clone https://github.com/cazzerson/Suma.git suma
+  sudo git clone https://github.com/cazzerson/Suma.git suma
 fi
-sudo docker-compose up -d
+
+if [ ! -d "suma/sumalogs" ]; then
+	mkdir suma/sumalogs
+	touch suma/sumalogs/sumaserver.log
+	chmod 666 suma/sumalogs/sumaserver.log
+fi
+
+cp configs/service/web/config/config.yaml suma/service/web/config/config.yaml
+cp configs/analysis/config/config.yaml suma/analysis/config/config.yaml
+cp configs/web/config/spaceassessConfig.js suma/web/config/spaceassessConfig.js
+
+#echo "Pulling containers"
+sudo docker-compose pull
+
+#echo "Running containers"
+docker-compose up -d --timeout 300
+
+cp configs/service/config/config.yaml suma/service/config/config.yaml
+sleep 10s
